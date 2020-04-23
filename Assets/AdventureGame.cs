6e09983d02d7cb6412306506e53ff5febea10930
mrbjasak@gmail.com <mrbjasak@gmail.com>
2020-04-23 23:59:42 +0200

    adding State

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private Text textComponent;
+    [SerializeField] private State startingState;
+
+    private State state;
+
+    private void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
